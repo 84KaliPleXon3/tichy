094ed10c27f5f0246ade303d19a2077868295174
guillaume <guillaume@hellokitty.(none)>
2008-11-08 18:19:27 +0800

    contacts: Added copyright

--- tichy/contacts/contacts.py
+++ tichy/contacts/contacts.py
@@ -1,3 +1,21 @@
+#    Tichy
+#
+#    copyright 2008 Guillaume Chereau (charlie@openmoko.org)
+#
+#    This file is part of Tichy.
+#
+#    Tichy is free software: you can redistribute it and/or modify it
+#    under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    Tichy is distributed in the hope that it will be useful, but
+#    WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#    General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with Tichy.  If not, see <http://www.gnu.org/licenses/>.
 
 from tichy.service import Service, ServiceUnusable
 from tichy.object import Object
